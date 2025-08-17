c31626800c22cadf2ed7185be9281791d039052c
creeoer <scienceaworld@gmail.com>
2025-08-16 22:31:23 -0400

    Updated nvim terminal config w/plugins & lazyvim

--- lua/regular-config.lua
+++ lua/regular-config.lua
@@ -1,14 +1,237 @@
--- Terminal Neovim configuration
--- Only loads when NOT in VSCode
+-- ~/.config/nvim/lua/regular-config.lua
 
--- Basic settings
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Basic Settings
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
 vim.opt.wrap = false
-vim.opt.scrolloff = 8
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 50
+vim.opt.cursorline = true
+vim.opt.clipboard = "unnamedplus"
 
--- Add any terminal-specific plugins or configs here
+-- Install Plugins
+require("lazy").setup({
+  -- 1. Color Scheme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin-mocha"
+    end,
+  },
+
+  -- 2. File Navigation
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules", ".git/" }
+        }
+      })
+    end,
+  },
+
+  -- 3. Syntax Highlighting
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "python", "markdown", "bash" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+
+  -- 4. File Explorer
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          width = 30,
+          side = "left",
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = false,
+        },
+      })
+    end,
+  },
+
+  -- 5. Git Integration
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup({
+        signs = {
+          add = { text = '│' },
+          change = { text = '│' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+      })
+    end,
+  },
+
+  -- 6. Status Line
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup({
+        options = {
+          theme = 'catppuccin',
+        },
+      })
+    end,
+  },
+
+  -- 7. Auto Pairs
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true,
+  },
+
+  -- 8. Comment Plugin
+  {
+    'numToStr/Comment.nvim',
+    config = true,
+  },
+
+  -- 9. GitHub Copilot
+  {
+    'github/copilot.vim',
+    -- Copilot will work with :Copilot setup on first use
+  },
+
+  -- 10. Which Key (shows keybindings)
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("which-key").setup()
+    end,
+  },
+
+  -- 11. Indent Lines
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = true,
+  },
+
+  -- 12. Better Escape
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup({
+        mapping = {"jk", "jj"},
+        timeout = 200,
+      })
+    end,
+  },
+})
+
+-- Keymaps
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- File Explorer
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- Telescope
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+keymap("n", "<leader>fh", ":Telescope help_tags<CR>", opts)
+keymap("n", "<leader>fr", ":Telescope oldfiles<CR>", opts)
+keymap("n", "<leader>gc", ":Telescope git_commits<CR>", opts)
+keymap("n", "<leader>gs", ":Telescope git_status<CR>", opts)
+
+-- Window Navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Window Splits
+keymap("n", "<leader>v", ":vsplit<CR>", opts)
+keymap("n", "<leader>h", ":split<CR>", opts)
+
+-- Buffer Navigation
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<leader>x", ":bdelete<CR>", opts)
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+-- Better paste
+keymap("v", "p", '"_dP', opts)
+
+-- Save file
+keymap("n", "<leader>w", ":w<CR>", opts)
+keymap("n", "<leader>W", ":wa<CR>", opts)
+
+-- Quit
+keymap("n", "<leader>q", ":q<CR>", opts)
+keymap("n", "<leader>Q", ":qa<CR>", opts)
+
+-- Clear search highlighting
+keymap("n", "<leader>/", ":nohlsearch<CR>", opts)
+
+-- Terminal
+keymap("n", "<leader>t", ":terminal<CR>", opts)
+keymap("t", "<Esc>", "<C-\\><C-n>", opts)
+
+-- Format
+keymap("n", "<leader>F", vim.lsp.buf.format, opts)
+
+-- Quick source current file
+keymap("n", "<leader><leader>", ":so %<CR>", opts)
