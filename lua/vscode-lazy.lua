c3da02bc66549be683f4744aa8569b3f4bc1d20c
creeoer <scienceaworld@gmail.com>
2025-08-17 21:39:49 -0400

    Disabled flash.nvim plugin

--- lua/vscode-lazy.lua
+++ lua/vscode-lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { "neovim/nvim-lspconfig",                 enabled = false },
     { "williamboman/mason.nvim",               enabled = false },
     { "hrsh7th/nvim-cmp",                      enabled = false },
+    { "folke/flash.nvim",                      enabled = false },
 }, {
     ui = { border = "rounded" },
 })
